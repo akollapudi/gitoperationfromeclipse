6e198dbe27d8b94e7461e32650ebb6c855423690
saisasank001 <saisasank001@gmail.com>
2016-05-17 20:56:46 -0400

    test2

--- src/gitdemo/Newfile.java
+++ src/gitdemo/Newfile.java
@@ -1,6 +1,11 @@
 package gitdemo;
 
 public class Newfile {
-	
+	public static void main(String args[]) {
+		try {
+			
+		System.out.println("Now the output is redirected!");
+		} catch(Exception e) {}
+	}
 
 }
