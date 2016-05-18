1ab4ea2d57622ccef1fd58405cac363c21db7d6a
akollapudi <akollapudi@miraclesoft.com>
2016-05-17 21:27:25 -0400

    conflict1

--- src/gitdemo/Newfile.java
+++ src/gitdemo/Newfile.java
@@ -2,16 +2,11 @@ package gitdemo;
 
 public class Newfile {
 	public static void main(String args[]) {
-		try {
+		
 			
 		System.out.println("Now the output is redirected!");
 		System.out.println("Now the output is redirected!");
-		} catch(Exception e) {}
-		System.out.println("Now the output is redirected!");
-		System.out.println("Now the output is redirected!");
 		
-		System.out.println("Now the output is redirected!");
-		System.out.println("Now !");
 	}
 
 }
